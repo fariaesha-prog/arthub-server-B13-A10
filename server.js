ae0ce323b76b5d51d596c763a782398c89f534bb
fariaesha-prog <fariaesha2003@gmail.com>
2026-06-22 18:14:28 +0600

    feat: deploying the server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  origin: [process.env.CLIENT_URL || "http://localhost:3000", "https://arthub-client-b13-a10.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
